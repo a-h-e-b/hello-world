4a78b89368d3172dc1ad417b068a870b3681aa74
Ashley <abarber6@email.cpcc.edu>
2021-02-06 22:11:14 -0500

    Create HelloWorld program
    Changed on IDE

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ public class HelloWorld
    public static void main(String [] args) 
    {
       System.out.println("Hello world!");
+      System.out.print("Program modified, but able to revert to original version 
+      System.out.println("using GitHub Desktop.");
    }
 }
