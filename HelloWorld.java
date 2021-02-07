07d8bb3f9dedf6d195554c18d5cb4e08abbf790d
Ashley <abarber6@email.cpcc.edu>
2021-02-06 22:13:55 -0500

    Revert "Create HelloWorld program"
    This reverts commit 4a78b89368d3172dc1ad417b068a870b3681aa74.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,5 @@ public class HelloWorld
    public static void main(String [] args) 
    {
       System.out.println("Hello world!");
-      System.out.print("Program modified, but able to revert to original version 
-      System.out.println("using GitHub Desktop.");
    }
 }
